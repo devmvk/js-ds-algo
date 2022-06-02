a69e888208fda2b71c112e6802f5e28ab50fe9c0
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-06-02 22:10:54 +0530

    adds problem statement comment

--- string/reverse_string_words.js
+++ string/reverse_string_words.js
@@ -1,3 +1,11 @@
+/*
+Reverse words in a given string
+https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1#
+
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+*/
+
 function reverseStringWords(value){
     return value.split(".").reverse().join('.')   
 }
