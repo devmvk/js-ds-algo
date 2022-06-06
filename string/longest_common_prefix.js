035fbf88d0692aa261d593e40c3e58cccfb9dc65
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-06-06 22:33:16 +0530

    longest common prefix code

--- string/longest_common_prefix.js
+++ string/longest_common_prefix.js
@@ -1,4 +1,21 @@
 function longestCommonPrefix(arr,n){
-
+    if(n == 0) return -1
+    let temp = arr[0]
+    for(let i=1; i< n; i++){
+        if(temp.length >= arr[i]){
+            for(let j = 0; j < arr[i]; j++){
+                if(temp.indexOf(arr[i]) > 0){
+                    temp = arr[i]
+                    break
+                }else{
+                    
+                }
+            }
+        }else{
+            for(let j = 0; j < temp.length; j++){
+                
+            }
+        }
+    }
 
 }
